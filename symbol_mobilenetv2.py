3a08b8a3e0ab798431ba377c79b94d2b5b581bdd
刘旭 <lx5@meitu.com>
2018-01-21 17:28:28 +0800

    change stride at Depthwise Convolution

--- symbol_mobilenetv2.py
+++ symbol_mobilenetv2.py
@@ -1,19 +1,18 @@
 import mxnet as mx
 '''
-    A MXNET implementation of mobilenetv2
+        A MXNET implementation of mobilenetv2
     use relu as Activation layer rather than relu6  
 '''
 def inverted_residual_unit(data, num_filter_input,num_filter_output,name,use_shortcut=True,stride=1, expansion_rate=1,bn_mom=0.9, workspace=256):
-    conv1 = mx.sym.Convolution(data=data, num_filter= num_filter_input, kernel=(1,1), stride=(stride,stride), pad=(0,0),
+    conv1 = mx.sym.Convolution(data=data, num_filter= num_filter_input, kernel=(1,1), stride=(1,1), pad=(0,0),
                                no_bias=True, workspace=workspace, name=name + '_pointwise_kernel_in')
     bn1 = mx.sym.BatchNorm(data=conv1, fix_gamma=False, eps=2e-5, momentum=bn_mom, name=name + '_bn1')
     act1 = mx.sym.Activation(data=bn1, act_type='relu', name=name + '_relu1')
-    conv2 = mx.sym.Convolution(data=act1, num_filter=int(num_filter_input*expansion_rate), kernel=(3,3), num_group=num_filter_input,stride=(1,1), pad=(1,1),
+    conv2 = mx.sym.Convolution(data=act1, num_filter=int(num_filter_input*expansion_rate), kernel=(3,3), num_group=num_filter_input,stride=(stride,stride), pad=(1,1),
                                no_bias=True, workspace=workspace, name=name + '_depthwise_kernel')
     bn2 = mx.sym.BatchNorm(data=conv2, fix_gamma=False, eps=2e-5, momentum=bn_mom, name=name + '_bn2')
     act2 = mx.sym.Activation(data=bn2, act_type='relu', name=name + '_relu2')
-    conv3 = mx.sym.Convolution(data=act2, num_filter=num_filter_output, kernel=(1,1), stride=(1,1), pad=(0,0), no_bias=True,
-                               workspace=workspace, name=name + '_pointwise_kernel_out')
+    conv3 = mx.sym.Convolution(data=act2, num_filter=num_filter_output, kernel=(1,1), stride=(1,1), pad=(0,0), no_bias=True,workspace=workspace, name=name + '_pointwise_kernel_out')
     bn3 = mx.sym.BatchNorm(data=conv3, fix_gamma=False, eps=2e-5, momentum=bn_mom, name=name + '_bn3')
 
     if use_shortcut:
@@ -42,8 +41,8 @@ def get_symbol(num_classes=1000, **kwargs):
             else:
                 body = inverted_residual_unit(body,filter_num_outputs[stage_num],filter_num_outputs[stage_num],\
                 'stage%d_level%d' % (stage_num + 2, level_num+1),expansion_rate=6)
-    stage7 = mx.sym.Convolution(data=body, num_filter=1280, kernel=(1, 1), no_bias=True, name='stage8_pointwise_kernel')
-    gpool = mx.symbol.Pooling(data=stage7, pool_type='avg', kernel=(7, 7),
+    stage8 = mx.sym.Convolution(data=body, num_filter=1280, kernel=(1, 1), no_bias=True, name='stage8_pointwise_kernel')
+    gpool = mx.symbol.Pooling(data=stage8, pool_type='avg', kernel=(7, 7),
                               global_pool=True, name='global_pool')
     conv8 = mx.symbol.Convolution(data=gpool, num_filter=num_classes, kernel=(1, 1),
                                   name='fc')
